777d74317a4ebd552bf875cafcb79fe77de329ff
cheesypoofscc <118968576+cheesypoofscc@users.noreply.github.com>
2026-07-08 15:51:54 +0800

    build.bat

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 
 call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
 
-cl /LD /O2 ImeState.cpp ole32.lib oleaut32.lib
+cl /LD /O2 /EHsc ImeState.cpp ole32.lib oleaut32.lib imm32.lib user32.lib
 
 
 pause
